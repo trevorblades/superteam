f330f387140227cd5de9c4539e90bdf293f16ef0
Trevor Blades <tdblades@gmail.com>
2019-02-26 22:37:09 -0800

    Add a header and share spacing values

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,47 @@ import Layout from '../components/layout';
 import PlayerCard from '../components/player-card';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import Typography from '@material-ui/core/Typography';
 import styled from '@emotion/styled';
+import theme from '@trevorblades/mui-theme';
+import withProps from 'recompose/withProps';
 import {TEAM_SIZE, TOTAL_BUDGET, getPlayerCardProps} from '../util';
 import {graphql} from 'gatsby';
 
+const spacing = 40;
+const halfSpacing = spacing / 2;
 const Container = styled.div({
-  padding: 40
+  padding: spacing
 });
+
+const Header = styled.header({
+  display: 'flex',
+  margin: `${halfSpacing}px 0 ${-halfSpacing}px`,
+  padding: `${halfSpacing}px ${spacing}px`,
+  position: 'sticky',
+  backgroundColor: theme.palette.background.default,
+  top: 0,
+  zIndex: 1
+});
+
+const HeaderItem = withProps({
+  component: 'button',
+  variant: 'h5'
+})(
+  styled(Typography)({
+    padding: 0,
+    border: 'none',
+    outline: 'none',
+    background: 'none',
+    cursor: 'pointer',
+    ':hover': {
+      color: theme.palette.text.hint
+    },
+    ':not(:last-child)': {
+      marginRight: 32
+    }
+  })
+);
 
 export default class App extends Component {
   static propTypes = {
@@ -53,8 +87,14 @@ export default class App extends Component {
     const isTeamFull = this.state.selectedPlayers.length >= TEAM_SIZE;
     return (
       <Layout>
+        <Header>
+          <HeaderItem>All players</HeaderItem>
+          <HeaderItem color="textSecondary">North America</HeaderItem>
+          <HeaderItem color="textSecondary">Europe</HeaderItem>
+          <HeaderItem color="textSecondary">Asia</HeaderItem>
+        </Header>
         <Container>
-          <Grid container spacing={40}>
+          <Grid container spacing={spacing}>
             {playerRanking.map(({player, rating}) => {
               const isSelected = this.isPlayerSelected(player);
               const {cost, percentile} = getPlayerCardProps(
