ec0cef3c80d85da3c0d0918a75817842d920051a
Trevor Blades <tdblades@gmail.com>
2019-03-20 12:49:42 -0700

    Pagewrap 404 page and add a netlify redirect config

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import Footer from '../components/footer';
 import Layout from '../components/layout';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import {Hero} from '../components/common';
+import {Hero, PageWrapper} from '../components/common';
 import {Link} from 'gatsby';
 
 // TODO: provide links to actions, like "create a team" etc.
@@ -10,12 +10,14 @@ export default function NotFound() {
   return (
     <Layout>
       <Hero>
-        <Typography variant="h2" gutterBottom>
-          Page not found
-        </Typography>
-        <Typography variant="body1">
-          Try starting over from <Link to="/">home</Link>.
-        </Typography>
+        <PageWrapper>
+          <Typography variant="h2" gutterBottom>
+            Page not found
+          </Typography>
+          <Typography variant="body1">
+            Try starting over from <Link to="/">home</Link>.
+          </Typography>
+        </PageWrapper>
       </Hero>
       <Footer />
     </Layout>
