7378be63c065cff299f197c060e2e4a319d324d2
Trevor Blades <tdblades@gmail.com>
2019-03-24 13:25:48 -0700

    Add terms and conditions checkbox

--- src/components/checkout-button/checkout-dialog.js
+++ src/components/checkout-button/checkout-dialog.js
@@ -1,13 +1,15 @@
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import LoginSaveButton from './login-save-button';
 import MoneyRow, {MoneyCell} from './money-row';
 import PlayerAvatar from '../player-avatar';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Component} from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -48,97 +50,136 @@ function updateEntries(cache, {data}) {
   }
 }
 
-function CheckoutDialog(props) {
-  const totalCost = getTotalPlayerCost(props.players);
-  return (
-    <Mutation
-      mutation={CREATE_ENTRY}
-      variables={{
-        playerIds: props.players.map(player => player.id)
-      }}
-      onCompleted={data => navigate(`/teams/${data.createEntry.id}`)}
-      update={updateEntries}
-    >
-      {(createTeam, {loading, error}) => (
-        <form
-          onSubmit={event => {
-            event.preventDefault();
+class CheckoutDialog extends Component {
+  static propTypes = {
+    onClose: PropTypes.func.isRequired,
+    players: PropTypes.array.isRequired,
+    user: PropTypes.object
+  };
 
-            // don't send the request if nobody is logged in
-            if (props.user) {
-              createTeam({
-                variables: {
-                  name: event.target.name.value
-                }
-              });
-            }
-          }}
-        >
-          <DialogTitle>Finalize your team</DialogTitle>
-          <DialogContent>
-            <DialogContentText gutterBottom>
-              That&apos;s a nice looking team! Now give it a name and save it to
-              enter the competition.
-            </DialogContentText>
-            <TextField
-              required
-              fullWidth
-              autoFocus
-              disabled={loading}
-              spellCheck={false}
-              autoComplete="off"
-              margin="normal"
-              label="Your team name"
-              variant="outlined"
-              name="name"
-              error={Boolean(error)}
-              helperText={error && error.message}
-            />
-            <Table padding="none">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Player name</TableCell>
-                  <TableCell align="right">Cost</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {props.players.map(player => (
-                  <TableRow key={player.id}>
-                    <TableCell>
-                      <PlayerCell>
-                        <PlayerAvatar player={player} size={32} />
-                        <PlayerName>{player.ign}</PlayerName>
-                      </PlayerCell>
-                    </TableCell>
-                    <MoneyCell value={getPlayerCost(player)} />
+  state = {
+    accepted: false
+  };
+
+  onAcceptedChange = event => {
+    this.setState({
+      accepted: event.target.checked
+    });
+  };
+
+  render() {
+    const totalCost = getTotalPlayerCost(this.props.players);
+    return (
+      <Mutation
+        mutation={CREATE_ENTRY}
+        variables={{
+          playerIds: this.props.players.map(player => player.id)
+        }}
+        onCompleted={data => navigate(`/teams/${data.createEntry.id}`)}
+        update={updateEntries}
+      >
+        {(createTeam, {loading, error}) => (
+          <form
+            onSubmit={event => {
+              event.preventDefault();
+
+              // don't send the request if nobody is logged in
+              if (this.props.user) {
+                createTeam({
+                  variables: {
+                    name: event.target.name.value
+                  }
+                });
+              }
+            }}
+          >
+            <DialogTitle>Finalize your team</DialogTitle>
+            <DialogContent>
+              <DialogContentText gutterBottom>
+                That&apos;s a nice looking team! Now give it a name, agree to
+                the terms, and save it to enter the contest.
+              </DialogContentText>
+              <TextField
+                required
+                fullWidth
+                autoFocus
+                disabled={loading}
+                spellCheck={false}
+                autoComplete="off"
+                margin="normal"
+                label="Your team name"
+                variant="outlined"
+                name="name"
+                error={Boolean(error)}
+                helperText={error && error.message}
+              />
+              <Table padding="none">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Player name</TableCell>
+                    <TableCell align="right">Cost</TableCell>
                   </TableRow>
-                ))}
-                <MoneyRow label="Total" value={totalCost} />
-                <MoneyRow label="Remainder" value={TOTAL_BUDGET - totalCost} />
-              </TableBody>
-            </Table>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={props.onClose} style={{marginRight: 'auto'}}>
-              <FaChevronLeft style={{marginRight: 8}} />
-              Go back
-            </Button>
-            {/*
+                </TableHead>
+                <TableBody>
+                  {this.props.players.map(player => (
+                    <TableRow key={player.id}>
+                      <TableCell>
+                        <PlayerCell>
+                          <PlayerAvatar player={player} size={32} />
+                          <PlayerName>{player.ign}</PlayerName>
+                        </PlayerCell>
+                      </TableCell>
+                      <MoneyCell value={getPlayerCost(player)} />
+                    </TableRow>
+                  ))}
+                  <MoneyRow label="Total" value={totalCost} />
+                  <MoneyRow
+                    label="Remainder"
+                    value={TOTAL_BUDGET - totalCost}
+                  />
+                </TableBody>
+              </Table>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    color="primary"
+                    checked={this.state.accepted}
+                    onChange={this.onAcceptedChange}
+                  />
+                }
+                label={
+                  <span>
+                    I have read and agree with the{' '}
+                    <a href="/terms" target="_blank" rel="noopener noreferrer">
+                      terms and conditions
+                    </a>
+                    .
+                  </span>
+                }
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={this.props.onClose}
+                style={{marginRight: 'auto'}}
+              >
+                <FaChevronLeft style={{marginRight: 8}} />
+                Go back
+              </Button>
+              {/*
               componentize login/save buttons to consume injected className prop
               more info: https://bit.ly/2SJyiXD
             */}
-            <LoginSaveButton user={props.user} disabled={loading} />
-          </DialogActions>
-        </form>
-      )}
-    </Mutation>
-  );
+              <LoginSaveButton
+                user={this.props.user}
+                disabled={!this.state.accepted || loading}
+              />
+            </DialogActions>
+          </form>
+        )}
+      </Mutation>
+    );
+  }
 }
-
-CheckoutDialog.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  players: PropTypes.array.isRequired,
-  user: PropTypes.object
-};
 
 export default withUser(CheckoutDialog);
