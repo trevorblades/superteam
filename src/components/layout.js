0e164f5d74a44fa39bcde2b1e43a44f290757e17
Trevor Blades <tdblades@gmail.com>
2019-03-24 11:57:39 -0700

    Add a custom favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,10 +35,6 @@ export default function Layout(props) {
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css?family=Inconsolata:400,700"
               />
-              <link
-                rel="icon"
-                href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/earth-globe-americas_1f30e.png"
-              />
             </Helmet>
             <Header />
             {props.children}
