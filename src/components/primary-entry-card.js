66220c391f6e4f1c9ce580e9e69b34d03e390a1b
Trevor Blades <tdblades@gmail.com>
2019-03-26 00:34:44 -0700

    More primary entry card to its own component

---
+++ src/components/primary-entry-card.js
@@ -0,0 +1,83 @@
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Grid from '@material-ui/core/Grid';
+import PlayerCard from './player-card';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import styled from '@emotion/styled';
+import withProps from 'recompose/withProps';
+import {EmptyPlayerCard} from './common';
+import {FaStar} from 'react-icons/fa';
+import {Link} from 'gatsby';
+import {MdShowChart} from 'react-icons/md';
+import {getEntryPlayers} from '../utils/get-entry-financials';
+import {withTheme} from '@material-ui/core/styles';
+
+const StyledStar = withTheme()(
+  styled(FaStar)(({theme}) => ({
+    display: 'block',
+    fill: theme.palette.primary.main
+  }))
+);
+
+const PlayerGridItem = withProps({
+  item: true,
+  md: 2,
+  sm: 3,
+  xs: 4
+})(Grid);
+
+const EmptyCardWrapper = styled(ButtonBase)({
+  width: '100%',
+  height: '100%'
+});
+
+const StyledEmptyPlayerCard = styled(EmptyPlayerCard)({
+  width: '100%',
+  height: '100%'
+});
+
+export default function PrimaryEntryCard(props) {
+  return (
+    <Card elevation={20}>
+      <CardHeader
+        avatar={<StyledStar size={32} />}
+        title={props.entry.name}
+        titleTypographyProps={{
+          variant: 'h6'
+        }}
+        subheader="Primary team"
+      />
+      <CardContent>
+        <Grid container spacing={16}>
+          {getEntryPlayers(props.entry).map(player => (
+            <PlayerGridItem key={player.id}>
+              <PlayerCard mini static selected player={player} />
+            </PlayerGridItem>
+          ))}
+          <PlayerGridItem>
+            <EmptyCardWrapper component={Link} to={`/teams/${props.entry.id}`}>
+              <StyledEmptyPlayerCard>
+                <MdShowChart size={32} />
+                <Typography
+                  variant="overline"
+                  color="inherit"
+                  style={{marginBottom: -8}}
+                >
+                  Details
+                </Typography>
+              </StyledEmptyPlayerCard>
+            </EmptyCardWrapper>
+          </PlayerGridItem>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+}
+
+PrimaryEntryCard.propTypes = {
+  entry: PropTypes.object.isRequired
+};
--- src/pages/teams.js
+++ src/pages/teams.js
@@ -1,20 +1,15 @@
 import AuthRequired from '../components/auth-required';
 import Button from '@material-ui/core/Button';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
 import EntryDrawer from '../components/entry-drawer';
 import FinancialCells, {FinancialHeaders} from '../components/financial-cells';
 import Footer from '../components/footer';
-import Grid from '@material-ui/core/Grid';
 import Helmet from 'react-helmet';
 import Layout from '../components/layout';
 import LoadingIndicator from '../components/loading-indicator';
 import NoIndex from '../components/no-index';
 import NoSsr from '@material-ui/core/NoSsr';
-import PlayerCard from '../components/player-card';
 import PrimaryCheckbox from '../components/primary-checkbox';
+import PrimaryEntryCard from '../components/primary-entry-card';
 import PropTypes from 'prop-types';
 import React, {Fragment} from 'react';
 import Table from '@material-ui/core/Table';
@@ -23,45 +18,15 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
-import getEntryFinancials, {
-  getEntryPlayers
-} from '../utils/get-entry-financials';
+import getEntryFinancials from '../utils/get-entry-financials';
 import styled from '@emotion/styled';
-import withProps from 'recompose/withProps';
-import {EmptyPlayerCard, PageWrapper, Section} from '../components/common';
-import {FaStar} from 'react-icons/fa';
 import {LIST_ENTRIES} from '../utils/queries';
 import {Link} from 'gatsby';
-import {MdShowChart} from 'react-icons/md';
+import {PageWrapper, Section} from '../components/common';
 import {Query} from 'react-apollo';
-import {withTheme} from '@material-ui/core/styles';
 
 const StyledPageWrapper = styled(PageWrapper)({
   padding: `${12}px 0`
-});
-
-const StyledStar = withTheme()(
-  styled(FaStar)(({theme}) => ({
-    display: 'block',
-    fill: theme.palette.primary.main
-  }))
-);
-
-const PlayerGridItem = withProps({
-  item: true,
-  md: 2,
-  sm: 3,
-  xs: 4
-})(Grid);
-
-const EmptyCardWrapper = styled(ButtonBase)({
-  width: '100%',
-  height: '100%'
-});
-
-const StyledEmptyPlayerCard = styled(EmptyPlayerCard)({
-  width: '100%',
-  height: '100%'
 });
 
 const title = 'My teams';
@@ -98,51 +63,12 @@ export default function Teams(props) {
                     );
                   }
 
-                  const primary = data.entries.find(entry => entry.primary);
                   return (
                     <Fragment>
                       <StyledPageWrapper mini>
-                        <Card elevation={20}>
-                          <CardHeader
-                            avatar={<StyledStar size={32} />}
-                            title={primary.name}
-                            titleTypographyProps={{
-                              variant: 'h6'
-                            }}
-                            subheader="Primary team"
-                          />
-                          <CardContent>
-                            <Grid container spacing={16}>
-                              {getEntryPlayers(primary).map(player => (
-                                <PlayerGridItem key={player.id}>
-                                  <PlayerCard
-                                    mini
-                                    static
-                                    selected
-                                    player={player}
-                                  />
-                                </PlayerGridItem>
-                              ))}
-                              <PlayerGridItem>
-                                <EmptyCardWrapper
-                                  component={Link}
-                                  to={`/teams/${primary.id}`}
-                                >
-                                  <StyledEmptyPlayerCard>
-                                    <MdShowChart size={32} />
-                                    <Typography
-                                      variant="overline"
-                                      color="inherit"
-                                      style={{marginBottom: -8}}
-                                    >
-                                      Details
-                                    </Typography>
-                                  </StyledEmptyPlayerCard>
-                                </EmptyCardWrapper>
-                              </PlayerGridItem>
-                            </Grid>
-                          </CardContent>
-                        </Card>
+                        <PrimaryEntryCard
+                          entry={data.entries.find(entry => entry.primary)}
+                        />
                       </StyledPageWrapper>
                       <br />
                       <Table padding="none">
