f87c34448ec8eb64acc9c2b863cc2139d058bf69
Trevor Blades <tdblades@gmail.com>
2019-04-02 17:12:39 -0700

    Add time to last updated date

--- src/components/last-updated.js
+++ src/components/last-updated.js
@@ -15,7 +15,7 @@ export default function LastUpdated() {
       `}
       render={data => {
         const date = new Date(data.site.siteMetadata.lastUpdated);
-        return <span>Last updated {date.toLocaleDateString()}</span>;
+        return <span>Last updated {date.toLocaleString()}</span>;
       }}
     />
   );
