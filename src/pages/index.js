3fd40de62cbbb84c4e01715ef9910e7ca9b88cf5
Trevor Blades <tdblades@gmail.com>
2020-02-07 17:17:07 -0800

    Add hover styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,8 @@ import {
   Grid,
   Heading,
   Image,
+  PseudoBox,
+  useColorMode,
   useTheme
 } from '@chakra-ui/core';
 import {ReactComponent as Logo} from '../assets/logo.svg';
@@ -17,6 +19,8 @@ import {graphql} from 'gatsby';
 
 export default function Index(props) {
   const {colors} = useTheme();
+  const {colorMode} = useColorMode();
+
   const classes = chroma
     .scale([
       colors.gray[500],
@@ -42,47 +46,60 @@ export default function Index(props) {
           content="Build a team of current and future CS:GO superstars and earn points based on your team's weekly performance."
         />
       </Helmet>
-      <Flex p="10" direction="column">
+      <Flex mx="auto" maxWidth="containers.xl" p="10" direction="column">
         <Flex align="center" mx="auto" mb="10">
-          <Box mr="3" as={Logo} w="16" h="16" />
-          <Heading as="h1" fontSize="4xl">
-            Superteam
-          </Heading>
+          <Box mr="3" as={Logo} w="12" h="12" />
+          <Heading fontSize="3xl">Superteam</Heading>
         </Flex>
-        <Grid
-          gap="6"
-          templateColumns={[
-            '1fr',
-            'repeat(2, 1fr)',
-            'repeat(3, 1fr)',
-            'repeat(4, 1fr)',
-            'repeat(5, 1fr)'
-          ]}
-        >
+        <Grid gap="6" templateColumns="repeat(auto-fit, minmax(250px, 1fr))">
           {players.map(player => {
             const score = (player.rating - minRating) / ratingDelta;
             const color = classes(score).hex();
             const {emoji} = emojiFlags.countryCode(player.country.code);
+
+            const bg = {
+              light: 'gray.50',
+              dark: 'gray.700'
+            };
+
+            const hoverBg = {
+              light: 'gray.100',
+              dark: 'gray.600'
+            };
+
             return (
               <AspectRatioBox
                 ratio={3 / 4}
                 key={player.id}
-                bgImg={`linear-gradient(${[color, 'white']})`}
-                bgPos="center"
-                bgSize="200%"
+                shadow="md"
+                cursor="pointer"
                 rounded="lg"
                 overflow="hidden"
+                role="group"
+                _hover={{
+                  shadow: 'xl'
+                }}
               >
-                <Flex direction="column">
+                <PseudoBox
+                  display="flex"
+                  flexDirection="column"
+                  bg={bg[colorMode]}
+                  bgImg={`linear-gradient(${[color, 'transparent']})`}
+                  bgPos="center"
+                  bgSize="200%"
+                  _groupHover={{
+                    bg: hoverBg[colorMode]
+                  }}
+                >
                   {player.team && (
                     <Image
-                      w="200%"
+                      w="175%"
                       maxW="none"
                       src={player.team.logo}
                       position="absolute"
                       top="50%"
-                      left="-50%"
-                      transform="translateY(-50%)"
+                      left="50%"
+                      transform="translate(-50%, -50%)"
                       opacity="0.5"
                       style={{mixBlendMode: 'luminosity'}}
                     />
@@ -102,11 +119,26 @@ export default function Index(props) {
                     bottom="0"
                     style={{mixBlendMode: 'overlay'}}
                   />
-                  <Box
-                    p="4"
+                  <PseudoBox
+                    as="svg"
                     mt="auto"
-                    bg="white"
-                    color="gray.800"
+                    viewBox="0 0 100 48"
+                    preserveAspectRatio="none"
+                    w="full"
+                    h="12"
+                    position="relative"
+                    fill="currentColor"
+                    color={bg[colorMode]}
+                    _groupHover={{
+                      color: hoverBg[colorMode]
+                    }}
+                  >
+                    <path d="M 0,48 Q 50,0 100,48 Z" />
+                  </PseudoBox>
+                  <Box
+                    p="5"
+                    pt="1"
+                    bg="inherit"
                     textAlign="center"
                     position="relative"
                   >
@@ -122,7 +154,7 @@ export default function Index(props) {
                       {emoji} {player.name}
                     </Heading>
                   </Box>
-                </Flex>
+                </PseudoBox>
               </AspectRatioBox>
             );
           })}
