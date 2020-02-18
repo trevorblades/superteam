5659e19356a359b0e391d27e66cb5943cff5b4a6
Trevor Blades <tdblades@gmail.com>
2020-02-17 17:57:46 -0800

    Add another breakpoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -188,7 +188,8 @@ export default function Index(props) {
               '1fr',
               'repeat(2, 1fr)',
               'repeat(3, 1fr)',
-              'repeat(4, 1fr)'
+              'repeat(4, 1fr)',
+              'repeat(5, 1fr)'
             ]}
           >
             {players
