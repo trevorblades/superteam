f2de625aa99c7cf5889eda759654f2f00f567d6c
Trevor Blades <tdblades@gmail.com>
2020-02-20 00:11:53 -0800

    Add comments about image creation

--- src/components/download-button.js
+++ src/components/download-button.js
@@ -3,13 +3,13 @@ import React, {Fragment, useRef, useState} from 'react';
 import logo from '../assets/logo.svg';
 import {Button, useTheme} from '@chakra-ui/core';
 
-function readImage(url) {
+function loadImage(url) {
   return new Promise(resolve => {
     const img = new Image();
-    img.crossOrigin = 'Anonymous'; // allow CORS-enabled images
     img.onload = () => {
       resolve(img);
     };
+    img.crossOrigin = 'Anonymous'; // allow CORS-enabled images
     img.src = url;
   });
 }
@@ -30,29 +30,35 @@ export default function DownloadButton(props) {
   async function handleClick() {
     setLoading(true);
 
-    const images = await Promise.all(
-      props.teamPlayers
-        .slice()
-        .sort((a, b) => a.rating - b.rating)
-        .map(player => readImage(player.image))
-    );
-
     const ctx = canvas.current.getContext('2d');
 
+    // create a gradient backdrop
     const gradient = ctx.createLinearGradient(400, 0, 400, 450);
     gradient.addColorStop(0, colors.gray[100]);
     gradient.addColorStop(1, colors.gray[400]);
 
+    // fill the background with the gradient
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, 800, 450);
 
+    // create loaded Image instances
+    const images = await Promise.all(
+      props.teamPlayers
+        .slice()
+        .sort((a, b) => a.rating - b.rating)
+        .map(player => loadImage(player.image))
+    );
+
+    // loop through images and draw them on the canvas
     images.forEach((img, index) => {
       ctx.drawImage(img, imagePositions[index], 33);
     });
 
-    const logoImg = await readImage(logo);
-    ctx.drawImage(logoImg, 24, 366, 60, 60);
+    // load the logo and draw it
+    const logoImg = await loadImage(logo);
+    ctx.drawImage(logoImg, 24, 366, 64, 64);
 
+    // simulate clicking on an anchor element with download attr
     const anchor = document.createElement('a');
     anchor.href = canvas.current.toDataURL('image/jpeg');
     anchor.download = 'superteam.jpg';
